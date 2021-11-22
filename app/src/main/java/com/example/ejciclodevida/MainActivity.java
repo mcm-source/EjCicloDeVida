7866fb987cd6b313a9cbb534682ff83e1567e009
a20martincm <martinmcm@hotmail.es>
2021-11-22 13:34:38 +0100

    Creacion metodos vida aplicacion

--- app/src/main/java/com/example/ejciclodevida/MainActivity.java
+++ app/src/main/java/com/example/ejciclodevida/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ejciclodevida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,41 +13,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Estamos en onCreate", Toast.LENGTH_LONG).show();
+        Log.i("ciclo","Estamos en onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(this, "Estamos en onStart", Toast.LENGTH_LONG).show();
+        Log.i("ciclo","Estamos en onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(this, "Estamos en onResume", Toast.LENGTH_LONG).show();
+        Log.i("ciclo","Estamos en onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "Estamos en onPause", Toast.LENGTH_LONG).show();
+        Log.i("ciclo","Estamos en onResume");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this, "Estamos en onStop", Toast.LENGTH_LONG).show();
+        Log.i("ciclo","Estamos en onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Toast.makeText(this, "Estamos en onRestart", Toast.LENGTH_LONG).show();
+        Log.i("ciclo","Estamos en onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "Estamos en onDestroy", Toast.LENGTH_LONG).show();
+        Log.i("ciclo","Estamos en onDestroy");
     }
 }
